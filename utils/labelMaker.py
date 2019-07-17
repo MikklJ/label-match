7f4f49decc7bcc8275f0f8c67b00d88867f5b04e
MikklJ <micheljim101@gmail.com>
2019-07-17 11:24:22 -0400

    Added labeling script

---
+++ utils/labelMaker.py
@@ -0,0 +1,51 @@
+"""
+Label applier for images
+Author: Michael Ji
+July 16, 2019
+"""
+
+import cv2
+import os
+import json
+
+# Root folder of the image files
+root = "/home/michael/datasets/michael_processed/"
+counter = 1
+outputLabels = {}
+
+
+class GetOutOfLoop(Exception):
+    pass
+
+
+try:
+    for dir in os.listdir(root):
+        for image in os.listdir(root + dir):
+            print("\nImage", counter, ":", dir + "/" + image)
+            counter += 1
+            label = ""
+            img = cv2.imread(root + dir + "/" + image)
+            # img_scaled = cv2.resize(img, (1000, 1000))
+            cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('image', 300, 300)
+
+            cv2.imshow('image', img)
+            k = cv2.waitKey(1)
+
+            label = input("Label: ")
+            if label == "ESC":
+                raise GetOutOfLoop
+            elif label != "":
+                print("Final Label =", label)
+                outputLabels.update({root + dir + "/" + image: label})
+            else:
+                print("SKIPPED", root + dir + "/" + image)
+except GetOutOfLoop:
+    pass
+
+print("\nDict values:")
+for key, value in outputLabels.items():
+    print(key, "with Label:", value)
+
+with open("labels.json", 'w') as json_file:
+    json.dump(outputLabels, json_file)
